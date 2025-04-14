1a0cb1f5a1788dac4f5e734e6c0343f51a03884f
smallfriends <1225006714@qq.com>
2025-04-14 22:06:09 +0800

    TreePractice二叉树练习

---
+++ src/main/java/hzm/lc/TreePractice.java
@@ -0,0 +1,29 @@
+package hzm.lc;
+
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class TreePractice {
+
+    public static void main(String[] args) {
+
+    }
+
+    //二叉树的中序遍历
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+    }
+
+}
